2e0422d3b97f4181adbc3839c9805f72b6e3ac1f
srino <seenivasan9559@gmail.com>
2021-03-27 10:52:41 +0530

    kjsa

--- cms/netlify/hooks/lifeCycle.js
+++ cms/netlify/hooks/lifeCycle.js
@@ -2,23 +2,23 @@ export default {
   beforeCreate() {},
   created() {},
   beforeMount() {},
-  mounted() {
-    const initNetlifyId = () => {
-      if (window.netlifyIdentity) {
-        window.netlifyIdentity.on('init', (user) => {
-          if (!user) {
-            window.netlifyIdentity.on('login', () => {
-              document.location.href = '/admin/'
-            })
-          }
-        })
-      }
-    }
-    initNetlifyId()
-    document
-      .getElementById('netlify-identity-widget-script')
-      .addEventListener('load', initNetlifyId)
-  },
+  // mounted() {
+  //   const initNetlifyId = () => {
+  //     if (window.netlifyIdentity) {
+  //       window.netlifyIdentity.on('init', (user) => {
+  //         if (!user) {
+  //           window.netlifyIdentity.on('login', () => {
+  //             document.location.href = '/admin/'
+  //           })
+  //         }
+  //       })
+  //     }
+  //   }
+  //   initNetlifyId()
+  //   document
+  //     .getElementById('netlify-identity-widget-script')
+  //     .addEventListener('load', initNetlifyId)
+  // },
   beforeUpdate() {},
   updated() {},
   beforeDestroy() {},
