63ba629e94df835330a94671bafff756418fd9b0
srino <seenivasan9559@gmail.com>
2021-03-27 08:57:24 +0530

    sadasadsa

--- config/head.js
+++ config/head.js
@@ -10,11 +10,11 @@ export default {
     }
   ],
   link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  script: [
-    {
-      src: 'https://identity.netlify.com/v1/netlify-identity-widget.js',
-      defer: true,
-      id: 'netlify-identity-widget-script'
-    }
-  ]
+  // script: [
+  //   {
+  //     src: 'https://identity.netlify.com/v1/netlify-identity-widget.js',
+  //     defer: true,
+  //     id: 'netlify-identity-widget-script'
+  //   }
+  // ]
 }
