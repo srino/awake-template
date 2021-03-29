e0b45a601ffea5b4b495961a35f4e8f50193efcd
srino <seenivasan9559@gmail.com>
2021-03-29 08:32:58 +0530

    jjjhhjj

--- cms/netlify/hooks/lifeCycle.js
+++ cms/netlify/hooks/lifeCycle.js
@@ -2,25 +2,22 @@ export default {
   beforeCreate() {},
   created() {},
   beforeMount() {},
-  // mounted() {
-  //   const initNetlifyId = () => {
-  //     if (window.netlifyIdentity) {
-  //       window.netlifyIdentity.on('init', (user) => {
-  //         if (!user) {
-  //           window.netlifyIdentity.on('login', () => {
-  //             document.location.href = '/admin/'
-  //           })
-  //         }
-  //       })
-  //     }
-  //   }
-  //   initNetlifyId()
-  //   document
-  //     .getElementById('netlify-identity-widget-script')
-  //     .addEventListener('load', initNetlifyId)
-  // },
   mounted() {
-    document.location.href = '/admin/'
+    const initNetlifyId = () => {
+      if (window.netlifyIdentity) {
+        window.netlifyIdentity.on('init', (user) => {
+          if (!user) {
+            window.netlifyIdentity.on('login', () => {
+              document.location.href = '/admin/'
+            })
+          }
+        })
+      }
+    }
+    initNetlifyId()
+    document
+      .getElementById('netlify-identity-widget-script')
+      .addEventListener('load', initNetlifyId)
   },
   beforeUpdate() {},
   updated() {},
